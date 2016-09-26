e442ac8210c1e1585b39731000804da7967448b1
Ryan Hungate <ryan@vextras.com>
2016-09-26 15:46:02 -0500

    removed key
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       MailChimp WooCommerce
  * Plugin URI:        https://woocommerce.mailchimpapp.com
  * Description:       MailChimp - WooCommerce plugin
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            MailChimp
  * Author URI:        https://woocommerce.mailchimpapp.com
  * License:           GPL-2.0+
@@ -38,7 +38,7 @@ function mailchimp_environment_variables() {
 		'repo' => 'feature/beta',
 		'environment' => 'beta',
 		'version' => '0.1.1',
-		'slack_token' => 'xoxp-2951297489-3735030724-3736784816-d135c4',
+		'slack_token' => false,
 		'slack_channel' => 'mc-woo',
 	);
 }

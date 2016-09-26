210bcc6d9e11bfa1abb30419032762c8b706e2d2
Ryan Hungate <ryan@vextras.com>
2016-09-26 16:20:33 -0500

    add support for admin order updates
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

--- includes/class-mailchimp-woocommerce-service.php
+++ includes/class-mailchimp-woocommerce-service.php
@@ -122,11 +122,11 @@ class MailChimp_Service extends MailChimp_Woocommerce_Options
      * @param bool $update Whether this is an existing post being updated or not.
      */
     public function handlePostSaved($post_id, $post, $update) {
-        if ('product' != $post->post_type) {
-            return;
+        if ('product' == $post->post_type) {
+            wp_queue(new MailChimp_WooCommerce_Single_Product($post_id), 5);
+        } elseif ('shop_order' == $post->post_type) {
+            wp_queue(new MailChimp_WooCommerce_Single_Order($post_id, null, null));
         }
-
-        wp_queue(new MailChimp_WooCommerce_Single_Product($post_id), 5);
     }
 
     /**

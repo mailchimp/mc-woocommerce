605b536b56d4964d62c16bab0d751e9708eef81d
Ryan Hungate <ryan@vextras.com>
2016-10-25 09:16:31 -0700

    changed version to 1.0.6 in plugin env

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       MailChimp for WooCommerce
  * Plugin URI:        https://mailchimp.com/connect-your-store/
  * Description:       MailChimp - WooCommerce plugin
- * Version:           1.0.5
+ * Version:           1.0.6
  * Author:            MailChimp
  * Author URI:        https://mailchimp.com
  * License:           GPL-2.0+
@@ -37,7 +37,7 @@ function mailchimp_environment_variables() {
 	return (object) array(
 		'repo' => 'master',
 		'environment' => 'production',
-		'version' => '1.0.5',
+		'version' => '1.0.6',
 		'slack_token' => false,
 		'slack_channel' => 'mc-woo',
 	);

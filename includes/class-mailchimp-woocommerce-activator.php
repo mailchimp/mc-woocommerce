efc10333602206f5fa81c058f2c322a192beecc4
brennen <brennen@ookla.com>
2016-12-03 16:18:57 -0800

    Fix install SQL statement

--- includes/class-mailchimp-woocommerce-activator.php
+++ includes/class-mailchimp-woocommerce-activator.php
@@ -117,7 +117,7 @@ class MailChimp_Woocommerce_Activator {
 		$sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}mailchimp_carts (
 				id VARCHAR (255) NOT NULL,
 				email VARCHAR (100) NOT NULL,
-				user_id INT (11) NULLABLE,
+				user_id INT (11) NULL,
                 cart text NOT NULL,
                 created_at datetime NOT NULL
 				) $charset_collate;";

17937a13d091f152db43eaa3a73852ce0a6787a2
Ryan Hungate <ryan@vextras.com>
2017-01-10 09:41:49 -0700

    update version to 1.0.73
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       MailChimp for WooCommerce
  * Plugin URI:        https://mailchimp.com/connect-your-store/
  * Description:       MailChimp - WooCommerce plugin
- * Version:           1.0.72
+ * Version:           1.0.73
  * Author:            MailChimp
  * Author URI:        https://mailchimp.com
  * License:           GPL-2.0+
@@ -39,7 +39,7 @@ function mailchimp_environment_variables() {
 	return (object) array(
 		'repo' => 'develop',
 		'environment' => 'production',
-		'version' => '1.0.72',
+		'version' => '1.0.73',
 		'wp_version' => (empty($wp_version) ? 'Unknown' : $wp_version),
 	);
 }

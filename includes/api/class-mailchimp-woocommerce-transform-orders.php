75380e9aebda05e70179e4ae7b5a3ae96f2950f9
Ryan Hungate <ryan@vextras.com>
2017-04-05 10:28:04 -0700

    add original order transformer for < 3.0 wc
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

--- includes/api/class-mailchimp-woocommerce-transform-orders.php
+++ includes/api/class-mailchimp-woocommerce-transform-orders.php
@@ -58,16 +58,16 @@ class MailChimp_WooCommerce_Transform_Orders
 
         $order = new MailChimp_WooCommerce_Order();
 
-        $order->setId($woo->get_id());
+        $order->setId($woo->id);
 
         // if we have a campaign id let's set it now.
         if (!empty($this->campaign_id)) {
             $order->setCampaignId($this->campaign_id);
         }
 
-        $order->setProcessedAt($woo->get_date_created());
+        $order->setProcessedAt(mailchimp_date_utc($woo->order_date));
 
-        $order->setCurrencyCode($woo->get_currency());
+        $order->setCurrencyCode($woo->get_order_currency());
 
         // grab the current statuses - this will end up being custom at some point.
         $statuses = $this->getOrderStatuses();
@@ -86,7 +86,7 @@ class MailChimp_WooCommerce_Transform_Orders
         $order->setFinancialStatus($financial_status);
 
         // only set this if the order is cancelled.
-        if ($status === 'cancelled') $order->setCancelledAt($woo->get_date_modified());
+        if ($status === 'cancelled') $order->setCancelledAt(mailchimp_date_utc($woo->modified_date));
 
         // set the total
         $order->setOrderTotal($woo->get_total());
@@ -98,7 +98,7 @@ class MailChimp_WooCommerce_Transform_Orders
         $order->setTaxTotal($woo->get_total_tax());
 
         // if we have shipping.
-        $order->setShippingTotal($woo->get_shipping_total());
+        $order->setShippingTotal($woo->get_total_shipping());
 
         // set the order discount
         $order->setDiscountTotal($woo->get_total_discount());
@@ -107,38 +107,34 @@ class MailChimp_WooCommerce_Transform_Orders
         $order->setCustomer($this->buildCustomerFromOrder($woo));
 
         // apply the addresses to the order
-        $order->setShippingAddress($this->transformShippingAddress($woo));
-        $order->setBillingAddress($this->transformBillingAddress($woo));
+        $addresses = $this->getOrderAddresses($woo);
+        $order->setShippingAddress($addresses->shipping);
+        $order->setBillingAddress($addresses->billing);
 
         // loop through all the order items
         foreach ($woo->get_items() as $key => $order_detail) {
 
-            /** @var WC_Order_Item_Product $order_detail */
-
             // add it into the order item container.
-            $item = $this->transformLineItem($key, $order_detail);
+            $item = $this->buildLineItem($key, $order_detail);
 
             // if we don't have a product post with this id, we need to add a deleted product to the MC side
-            if (!($product = $order_detail->get_product())) {
-
-                $pid = $order_detail->get_product_id();
-
-                if (empty($pid)) {
-                    continue;
-                }
+            if (!($product_post = get_post($item->getProductId()))) {
 
                 // check if it exists, otherwise create a new one.
-                if (($deleted_product = MailChimp_WooCommerce_Transform_Products::deleted($pid))) {
+                if (($deleted_product = MailChimp_WooCommerce_Transform_Products::deleted($item->getProductId()))) {
+
+                    $deleted_product_id = "deleted_{$item->getProductId()}";
+
                     // swap out the old item id and product variant id with the deleted version.
-                    $item->setProductId("deleted_{$pid}");
-                    $item->setProductVariantId("deleted_{$pid}");
+                    $item->setProductId($deleted_product_id);
+                    $item->setProductVariantId($deleted_product_id);
 
                     // add the item and continue on the loop.
                     $order->addItem($item);
                     continue;
                 }
 
-                mailchimp_log('order.items.error', "Order #{$woo->get_id()} :: Product {$pid} does not exist!");
+                mailchimp_log('order.items.error', "Order #{$woo->id} :: Product {$item->getProductId()} does not exist!");
                 continue;
             }
 
@@ -146,7 +142,7 @@ class MailChimp_WooCommerce_Transform_Orders
         }
 
         //if (($refund = $woo->get_total_refunded()) && $refund > 0){
-            // this is where we would be altering the submission to tell us about the refund.
+        // this is where we would be altering the submission to tell us about the refund.
         //}
 
         return $order;
@@ -160,43 +156,145 @@ class MailChimp_WooCommerce_Transform_Orders
     {
         $customer = new MailChimp_WooCommerce_Customer();
 
-        $customer->setId(md5(trim(strtolower($order->get_billing_email()))));
-        $customer->setCompany($order->get_billing_company());
-        $customer->setEmailAddress(trim($order->get_billing_email()));
-        $customer->setFirstName($order->get_billing_first_name());
-        $customer->setLastName($order->get_billing_last_name());
-        $customer->setAddress($this->transformBillingAddress($order));
-
-        if (!($stats = $this->getCustomerOrderTotals($order->get_user_id()))) {
-            $stats = (object) array('count' => 0, 'total' => 0);
-        }
-        $customer->setOrdersCount($stats->count);
-        $customer->setTotalSpent($stats->total);
+        $customer->setId(md5(trim(strtolower($order->billing_email))));
+        $customer->setCompany($order->billing_company);
+        $customer->setEmailAddress(trim($order->billing_email));
+        $customer->setFirstName($order->billing_first_name);
+        $customer->setLastName($order->billing_last_name);
+        $customer->setOrdersCount(1);
+        $customer->setTotalSpent($order->get_total());
 
         // we are saving the post meta for subscribers on each order... so if they have subscribed on checkout
-        $subscriber_meta = get_post_meta($order->get_id(), 'mailchimp_woocommerce_is_subscribed', true);
+        $subscriber_meta = get_post_meta($order->id, 'mailchimp_woocommerce_is_subscribed', true);
         $subscribed_on_order = $subscriber_meta === '' ? false : (bool) $subscriber_meta;
 
         $customer->setOptInStatus($subscribed_on_order);
+
+        // use the info from the order to compile an address.
+        $address = new MailChimp_WooCommerce_Address();
+        $address->setAddress1($order->billing_address_1);
+        $address->setAddress2($order->billing_address_2);
+        $address->setCity($order->billing_city);
+        $address->setProvince($order->billing_state);
+        $address->setPostalCode($order->billing_postcode);
+        $address->setCountry($order->billing_country);
+        $address->setPhone($order->billing_phone);
+
+        // if we have billing names set it here
+        if (!empty($order->billing_first_name) && !empty($order->billing_last_name)) {
+            $address->setName($order->billing_first_name.' '.$order->billing_last_name);
+        }
+
+        $customer->setAddress($address);
+
+        if (($user = get_userdata($order->customer_user))) {
+
+            /** IF we wanted to use the user data instead we would do it here.
+             * but we discussed using the billing address instead.
+             */
+
+            /*
+            $customer->setId($user->ID);
+            $customer->setEmailAddress($user->user_email);
+            $customer->setFirstName($user->first_name);
+            $customer->setLastName($user->last_name);
+
+            if (($address = $this->getUserAddress($user->ID))) {
+                if (count($address->toArray()) > 3) {
+                    $customer->setAddress($address);
+                }
+            }
+            */
+
+            if (!($stats = $this->getCustomerOrderTotals($order->customer_user))) {
+                $stats = (object) array('count' => 0, 'total' => 0);
+            }
+
+            $customer->setOrdersCount($stats->count);
+            $customer->setTotalSpent($stats->total);
+        }
 
         return $customer;
     }
 
     /**
      * @param $key
-     * @param WC_Order_Item_Product $order_detail
+     * @param $order_detail
      * @return MailChimp_WooCommerce_LineItem
      */
-    protected function transformLineItem($key, $order_detail)
+    protected function buildLineItem($key, $order_detail)
     {
         // fire up a new MC line item
         $item = new MailChimp_WooCommerce_LineItem();
         $item->setId($key);
 
-        $item->setPrice($order_detail->get_total());
-        $item->setProductId($order_detail->get_product_id());
-        $item->setProductVariantId($order_detail->get_variation_id());
-        $item->setQuantity($order_detail->get_quantity());
+        if (isset($order_detail['item_meta']) && is_array($order_detail['item_meta'])) {
+
+            foreach ($order_detail['item_meta'] as $meta_key => $meta_data_array) {
+
+                if (!isset($meta_data_array[0])) {
+                    continue;
+                }
+
+                switch ($meta_key) {
+
+                    case '_line_subtotal':
+                        $item->setPrice($meta_data_array[0]);
+                        break;
+
+                    case '_product_id':
+                        $item->setProductId($meta_data_array[0]);
+                        break;
+
+                    case '_variation_id':
+                        $item->setProductVariantId($meta_data_array[0]);
+                        break;
+
+                    case '_qty':
+                        $item->setQuantity($meta_data_array[0]);
+                        break;
+
+                }
+            }
+
+            if ($item->getProductVariantId() <= 0) {
+                $item->setProductVariantId($item->getProductId());
+            }
+
+        } elseif (isset($order_detail['item_meta_array']) && is_array($order_detail['item_meta_array'])) {
+
+            /// Some users have the newer version of the item meta.
+
+            foreach ($order_detail['item_meta_array'] as $meta_id => $object) {
+
+                if (!isset($object->key)) {
+                    continue;
+                }
+
+                switch ($object->key) {
+
+                    case '_line_subtotal':
+                        $item->setPrice($object->value);
+                        break;
+
+                    case '_product_id':
+                        $item->setProductId($object->value);
+                        break;
+
+                    case '_variation_id':
+                        $item->setProductVariantId($object->value);
+                        break;
+
+                    case '_qty':
+                        $item->setQuantity($object->value);
+                        break;
+                }
+            }
+
+            if ($item->getProductVariantId() <= 0) {
+                $item->setProductVariantId($item->getProductId());
+            }
+        }
 
         if ($item->getQuantity() > 1) {
             $current_price = $item->getPrice();
@@ -208,62 +306,6 @@ class MailChimp_WooCommerce_Transform_Orders
     }
 
     /**
-     * @param WC_Order $order
-     * @return MailChimp_WooCommerce_Address
-     */
-    public function transformBillingAddress(WC_Order $order)
-    {
-        // use the info from the order to compile an address.
-        $address = new MailChimp_WooCommerce_Address();
-        $address->setAddress1($order->get_billing_address_1());
-        $address->setAddress2($order->get_billing_address_2());
-        $address->setCity($order->get_billing_city());
-        $address->setProvince($order->get_billing_state());
-        $address->setPostalCode($order->get_billing_postcode());
-        $address->setCountry($order->get_billing_country());
-        $address->setPhone($order->get_billing_phone());
-
-        $bfn = $order->get_billing_first_name();
-        $bln = $order->get_billing_last_name();
-
-        // if we have billing names set it here
-        if (!empty($bfn) && !empty($bln)) {
-            $address->setName("{$bfn} {$bln}");
-        }
-
-        return $address;
-    }
-
-    /**
-     * @param WC_Order $order
-     * @return MailChimp_WooCommerce_Address
-     */
-    public function transformShippingAddress(WC_Order $order)
-    {
-        $address = new MailChimp_WooCommerce_Address();
-
-        $address->setAddress1($order->get_shipping_address_1());
-        $address->setAddress2($order->get_shipping_address_2());
-        $address->setCity($order->get_shipping_city());
-        $address->setProvince($order->get_shipping_state());
-        $address->setPostalCode($order->get_shipping_postcode());
-        $address->setCountry($order->get_shipping_country());
-
-        // shipping does not have a phone number, so maybe use this?
-        $address->setPhone($order->get_billing_phone());
-
-        $sfn = $order->get_billing_first_name();
-        $sln = $order->get_billing_last_name();
-
-        // if we have billing names set it here
-        if (!empty($sfn) && !empty($sln)) {
-            $address->setName("{$sfn} {$sln}");
-        }
-
-        return $address;
-    }
-
-    /**
      * @param int $page
      * @param int $posts
      * @return array|bool
@@ -271,8 +313,8 @@ class MailChimp_WooCommerce_Transform_Orders
     public function getOrderPosts($page = 1, $posts = 10)
     {
         $orders = get_posts(array(
-            'post_type'   => wc_get_order_types(),
-            'post_status' => array_keys(wc_get_order_statuses()),
+            'post_type' => 'shop_order',
+            //'post_status' => 'publish',
             'posts_per_page' => $posts,
             'paged' => $page,
             'orderby' => 'id',
@@ -294,14 +336,100 @@ class MailChimp_WooCommerce_Transform_Orders
      */
     public function getCustomerOrderTotals($user_id)
     {
-        $customer = new WC_Customer($user_id);
-        $customer->get_order_count();
-        $customer->get_total_spent();
+        $stats = (object) array('count' => 0, 'total' => 0);
 
-        return (object) array(
-            'count' => $customer->get_order_count(),
-            'total' => $customer->get_total_spent()
+        if (!empty($user_id)) {
+            $orders = get_posts(apply_filters('woocommerce_my_account_my_orders_query', array(
+                'numberposts' => -1,
+                'meta_key'    => '_customer_user',
+                'meta_value'  => $user_id,
+                'post_type'   => 'shop_order',
+                'post_status' => 'publish'
+            )));
+
+            foreach ($orders as $order) {
+                $woo = new WC_Order($order);
+                $stats->total += $woo->get_total();
+                $stats->count++;
+            }
+            return $stats;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param WC_Order $order
+     * @return object
+     */
+    public function getOrderAddresses(WC_Order $order)
+    {
+        // use the info from the order to compile an address.
+        $billing = new MailChimp_WooCommerce_Address();
+        $billing->setAddress1($order->billing_address_1);
+        $billing->setAddress2($order->billing_address_2);
+        $billing->setCity($order->billing_city);
+        $billing->setProvince($order->billing_state);
+        $billing->setPostalCode($order->billing_postcode);
+        $billing->setCountry($order->billing_country);
+        $billing->setPhone($order->billing_phone);
+
+        // if we have billing names go ahead and apply them
+        if (!empty($order->billing_first_name) && !empty($order->billing_last_name)) {
+            $billing->setName($order->billing_first_name.' '.$order->billing_last_name);
+        }
+
+        $shipping = new MailChimp_WooCommerce_Address();
+        $shipping->setAddress1($order->shipping_address_1);
+        $shipping->setAddress2($order->shipping_address_2);
+        $shipping->setCity($order->shipping_city);
+        $shipping->setProvince($order->shipping_state);
+        $shipping->setPostalCode($order->shipping_postcode);
+        $shipping->setCountry($order->shipping_country);
+        if (isset($order->shipping_phone)) {
+            $shipping->setPhone($order->shipping_phone);
+        }
+
+        // if we have shipping names go ahead and apply them
+        if (!empty($order->shipping_first_name) && !empty($order->shipping_last_name)) {
+            $shipping->setName($order->shipping_first_name.' '.$order->shipping_last_name);
+        }
+
+        return (object) array('billing' => $billing, 'shipping' => $shipping);
+    }
+
+    /**
+     * @param $user_id
+     * @param string $type
+     * @return MailChimp_WooCommerce_Address
+     */
+    public function getUserAddress($user_id, $type = 'billing')
+    {
+        $address = new MailChimp_WooCommerce_Address();
+
+        // pull all the meta for this user.
+        $meta = get_user_meta($user_id);
+
+        // loop through all the possible address properties, and if we have on on the user, set the property
+        // because it's more up to date.
+        $address_props = array(
+            $type.'_address_1' => 'setAddress1',
+            $type.'_address_2' => 'setAddress2',
+            $type.'_city' => 'setCity',
+            $type.'_state' => 'setProvince',
+            $type.'_postcode' => 'setPostalCode',
+            $type.'_country' => 'setCountry',
+            $type.'_phone' => 'setPhone',
         );
+
+        // loop through all the address properties and set the values if we have one.
+        foreach ($address_props as $address_key => $address_call) {
+            if (isset($meta[$address_key]) && !empty($meta[$address_key]) && isset($meta[$address_key][0])) {
+                $address->$address_call($meta[$address_key][0]);
+            }
+        }
+
+        return $address;
     }
 
     /**

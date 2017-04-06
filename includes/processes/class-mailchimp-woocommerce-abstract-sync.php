5d68f13f149d4ade265a4c4db8a7d26ef726bce8
Ryan Hungate <ryan@vextras.com>
2017-04-05 17:15:54 -0700

    move data pointers up
    Signed-off-by: Ryan Hungate <ryan@vextras.com>

--- includes/processes/class-mailchimp-woocommerce-abstract-sync.php
+++ includes/processes/class-mailchimp-woocommerce-abstract-sync.php
@@ -155,8 +155,6 @@ abstract class MailChimp_WooCommerce_Abtstract_Sync extends WP_Job
      */
     public function flagStopSync()
     {
-        mailchimp_log('sync.completed', "Finished Sync :: ".date('D, M j, Y g:i A'));
-
         // this is the last thing we're doing so it's complete as of now.
         $this->setData('sync.syncing', false);
         $this->setData('sync.completed_at', time());
@@ -164,6 +162,8 @@ abstract class MailChimp_WooCommerce_Abtstract_Sync extends WP_Job
         // set the current sync pages back to 1 if the user hits resync.
         $this->setData('sync.orders.current_page', 1);
         $this->setData('sync.products.current_page', 1);
+
+        mailchimp_log('sync.completed', "Finished Sync :: ".date('D, M j, Y g:i A'));
 
         // flag the store as sync_finished
         mailchimp_get_api()->flagStoreSync(mailchimp_get_store_id(), false);

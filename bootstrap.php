94f930241bdd4f29ac92a206bbac1b958bffe8da
Pedro Germani <pedro.germani@gmail.com>
2019-02-15 15:54:27 -0200

    2.1.15 Update plugin version

--- bootstrap.php
+++ bootstrap.php
@@ -88,7 +88,7 @@ function mailchimp_environment_variables() {
     return (object) array(
         'repo' => 'master',
         'environment' => 'production',
-        'version' => '2.1.14',
+        'version' => '2.1.15',
         'php_version' => phpversion(),
         'wp_version' => (empty($wp_version) ? 'Unknown' : $wp_version),
         'wc_version' => function_exists('WC') ? WC()->version : null,

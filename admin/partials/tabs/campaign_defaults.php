e1655033f886467bdb22cc34ab156da4364c00b1
Pedro Germani <pedro.germani@gmail.com>
2019-05-24 13:00:36 +0200

    2.2 Re-skin: Store settings and audience defaults

--- admin/partials/tabs/campaign_defaults.php
+++ admin/partials/tabs/campaign_defaults.php
@@ -13,15 +13,11 @@ if (!$handler->hasValidStoreInfo()) {
 
 <input type="hidden" name="mailchimp_active_settings_tab" value="campaign_defaults"/>
 
-<h2 style="padding-top: 1em;"><?php _e('Audience Defaults', $this->plugin_name);?></h2>
-<p><?php esc_html_e('Please fill out the default campaign information.', $this->plugin_name);?></p>
-
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Contact Name', $this->plugin_name)?></span>
     </legend>
-    <label for="<?php echo $this->plugin_name; ?>-campaign-from-name-label">
-        <input style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-campaign-from-name-label" name="<?php echo $this->plugin_name; ?>[campaign_from_name]" value="<?php echo isset($options['campaign_from_name']) ? $options['campaign_from_name'] : '' ?>" required/>
+    <label for="<?php echo $this->plugin_name; ?>-campaign-from-name-label"> 
         <?php
             esc_html_e('Default from name', $this->plugin_name);
             if (empty($options['campaign_from_name']) ) {
@@ -29,75 +25,79 @@ if (!$handler->hasValidStoreInfo()) {
             }
         ?>
     </label>
+    <input type="text" id="<?php echo $this->plugin_name; ?>-campaign-from-name-label" name="<?php echo $this->plugin_name; ?>[campaign_from_name]" value="<?php echo isset($options['campaign_from_name']) ? $options['campaign_from_name'] : '' ?>" required/>
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('From Email', $this->plugin_name)?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-campaign-from-email-label">
-        <input style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-campaign-from-email-label" name="<?php echo $this->plugin_name; ?>[campaign_from_email]" value="<?php echo isset($options['campaign_from_email']) ? $options['campaign_from_email'] : get_option('admin_email') ?>" required/>
-        <?php
+            <?php
             esc_html_e('Default from email', $this->plugin_name);
             if (empty($options['campaign_from_email']) ) {
                 echo '<span style="color:red;">*</span>';
             }
-        ?>
+            ?>
     </label>
+    <input type="text" id="<?php echo $this->plugin_name; ?>-campaign-from-email-label" name="<?php echo $this->plugin_name; ?>[campaign_from_email]" value="<?php echo isset($options['campaign_from_email']) ? $options['campaign_from_email'] : get_option('admin_email') ?>" required/>
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Default subject', $this->plugin_name)?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-campaign-subject-label">
-        <input style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-campaign-subject-label" name="<?php echo $this->plugin_name; ?>[campaign_subject]" value="<?php echo isset($options['campaign_subject']) ? $options['campaign_subject'] : get_option('blogname') ?>" required/>
-        <?php
+            <?php
             esc_html_e('Default subject', $this->plugin_name);
             if (empty($options['campaign_subject']) ) {
                 echo '<span style="color:red;">*</span>';
             }
-        ?>
+            ?>
     </label>
+    <input type="text" id="<?php echo $this->plugin_name; ?>-campaign-subject-label" name="<?php echo $this->plugin_name; ?>[campaign_subject]" value="<?php echo isset($options['campaign_subject']) ? $options['campaign_subject'] : get_option('blogname') ?>" required/>
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Default language', $this->plugin_name)?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-campaign-language-label">
-        <select id="<?php echo $this->plugin_name; ?>-campaign-language-label" name="<?php echo $this->plugin_name; ?>[campaign_language]" style="width:30%" required>
+            <?php
+            esc_html_e('Default language', $this->plugin_name);
+            if (empty($options['campaign_language']) ) {
+                echo '<span style="color:red;">*</span>';
+            }
+            ?>
+    </label>
+    <div class="mailchimp-select-wrapper">
+        <select id="<?php echo $this->plugin_name; ?>-campaign-language-label" name="<?php echo $this->plugin_name; ?>[campaign_language]" required>
             <?php $selected_locale = isset($options['campaign_language']) && !empty($options['campaign_language']) ? $options['campaign_language'] : 'en'; ?>
             <?php
             foreach(MailChimp_Api_Locales::simple() as $locale_key => $local_value) {
                 echo '<option value="' . esc_attr( $locale_key ) . '" ' . selected($locale_key === $selected_locale, true, false ) . '>' . esc_html( $local_value ) . '</option>';
             }
             ?>
-        </select>
-        <?php
-            esc_html_e('Default language', $this->plugin_name);
-            if (empty($options['campaign_language']) ) {
-                echo '<span style="color:red;">*</span>';
-            }
-        ?>
-    </label>
+        </select>    
+    </div>
+    
+    
 </fieldset>
 
-<fieldset>
+<fieldset class="full">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Permission Reminder', $this->plugin_name)?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-campaign-permission-reminder-label">
-        <textarea 
-            style="width: 30%;"
-            id="<?php echo $this->plugin_name; ?>-campaign-permission-reminder-label"
-            name="<?php echo $this->plugin_name; ?>[campaign_permission_reminder]"
-            required><?php echo isset($options['campaign_permission_reminder']) ? $options['campaign_permission_reminder'] : sprintf(/* translators: %s - plugin name. */esc_html__( 'You were subscribed to the newsletter from %s', $this->plugin_name ),get_option('blogname'));?></textarea>
-        <?php
+            <?php
             esc_html_e('Permission reminder message', $this->plugin_name);
             if (empty($options['campaign_permission_reminder']) ) {
                 echo '<span style="color:red;">*</span>';
             }
-        ?>
+            ?>
     </label>
+    <textarea 
+        id="<?php echo $this->plugin_name; ?>-campaign-permission-reminder-label"
+        name="<?php echo $this->plugin_name; ?>[campaign_permission_reminder]"
+        required><?php echo isset($options['campaign_permission_reminder']) ? $options['campaign_permission_reminder'] : sprintf(/* translators: %s - plugin name. */esc_html__( 'You were subscribed to the newsletter from %s', $this->plugin_name ),get_option('blogname'));?></textarea>
 </fieldset>
--- admin/partials/tabs/store_info.php
+++ admin/partials/tabs/store_info.php
@@ -10,17 +10,13 @@ if (!$handler->hasValidApiKey()) {
 ?>
 <input type="hidden" name="mailchimp_active_settings_tab" value="store_info"/>
 
-<h2 style="padding-top: 1em;"><?php _e('Store Settings', $this->plugin_name);?></h2>
-<p><?= __('Please provide the following information about your WooCommerce store.', $this->plugin_name); ?></p>
-
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Store Name', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-name-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-name-label" name="<?php echo $this->plugin_name; ?>[store_name]" value="<?php echo isset($options['store_name']) ? $options['store_name'] : get_option('blogname') ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_name']) ) {
                 esc_html_e('Name', $this->plugin_name);
             } else {
@@ -29,16 +25,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-name-label" name="<?php echo $this->plugin_name; ?>[store_name]" value="<?php echo isset($options['store_name']) ? $options['store_name'] : get_option('blogname') ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Email', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-admin-email-label">
-        <input required style="width: 30%;" type="email" id="<?php echo $this->plugin_name; ?>-admin-email-label" name="<?php echo $this->plugin_name; ?>[admin_email]" value="<?php echo isset($options['admin_email']) ? $options['admin_email'] : get_option('admin_email') ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['admin_email']) ) {
                 esc_html_e('Email', $this->plugin_name);
             } else {
@@ -47,16 +43,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="email" id="<?php echo $this->plugin_name; ?>-admin-email-label" name="<?php echo $this->plugin_name; ?>[admin_email]" value="<?php echo isset($options['admin_email']) ? $options['admin_email'] : get_option('admin_email') ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Street Address', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-address-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-address-label" name="<?php echo $this->plugin_name; ?>[store_street]" value="<?php echo isset($options['store_street']) ? $options['store_street'] : '' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_street']) ) {
                 esc_html_e('Street address', $this->plugin_name);
             } else {
@@ -65,16 +61,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-address-label" name="<?php echo $this->plugin_name; ?>[store_street]" value="<?php echo isset($options['store_street']) ? $options['store_street'] : '' ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('City', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-city-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-city-label" name="<?php echo $this->plugin_name; ?>[store_city]" value="<?php echo isset($options['store_city']) ? $options['store_city'] : '' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_city']) ) {
                 esc_html_e('City', $this->plugin_name);
             } else {
@@ -83,16 +79,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-city-label" name="<?php echo $this->plugin_name; ?>[store_city]" value="<?php echo isset($options['store_city']) ? $options['store_city'] : '' ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('State', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-state-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-state-label" name="<?php echo $this->plugin_name; ?>[store_state]" value="<?php echo isset($options['store_state']) ? $options['store_state'] : '' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_state']) ) {
                 esc_html_e('State', $this->plugin_name);
             } else {
@@ -101,16 +97,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-state-label" name="<?php echo $this->plugin_name; ?>[store_state]" value="<?php echo isset($options['store_state']) ? $options['store_state'] : '' ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Postal Code', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-postal-code-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-postal-code-label" name="<?php echo $this->plugin_name; ?>[store_postal_code]" value="<?php echo isset($options['store_postal_code']) ? $options['store_postal_code'] : '' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_postal_code']) ) {
                 esc_html_e('Postal Code', $this->plugin_name);
             } else {
@@ -119,16 +115,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-postal-code-label" name="<?php echo $this->plugin_name; ?>[store_postal_code]" value="<?php echo isset($options['store_postal_code']) ? $options['store_postal_code'] : '' ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Country', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-country-label">
-        <input required style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-country-label" name="<?php echo $this->plugin_name; ?>[store_country]" value="<?php echo isset($options['store_country']) ? $options['store_country'] : 'US' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_country'])) {
                 esc_html_e('Country', $this->plugin_name);
             } else {
@@ -137,16 +133,16 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input required type="text" id="<?php echo $this->plugin_name; ?>-store-country-label" name="<?php echo $this->plugin_name; ?>[store_country]" value="<?php echo isset($options['store_country']) ? $options['store_country'] : 'US' ?>" />
 </fieldset>
 
-<fieldset>
+<fieldset class="half">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Phone Number', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-phone-label">
-        <input style="width: 30%;" type="text" id="<?php echo $this->plugin_name; ?>-store-phone-label" name="<?php echo $this->plugin_name; ?>[store_phone]" value="<?php echo isset($options['store_phone']) ? $options['store_phone'] : '' ?>" />
-        <span>
-            <?php
+            <span>
+                <?php
             if (!empty($options['store_phone']) ) {
                 esc_html_e('Phone Number', $this->plugin_name);
             } else {
@@ -155,18 +151,22 @@ if (!$handler->hasValidApiKey()) {
             ?>
         </span>
     </label>
+    <input type="text" id="<?php echo $this->plugin_name; ?>-store-phone-label" name="<?php echo $this->plugin_name; ?>[store_phone]" value="<?php echo isset($options['store_phone']) ? $options['store_phone'] : '' ?>" />
 </fieldset>
 
 <h2 style="padding-top: 1em;"><?= __('Locale Settings', $this->plugin_name);?></h2>
 
 <p><?= __('Please apply your locale settings. If you\'re unsure about these, use the defaults.', $this->plugin_name);?></p>
 
-<fieldset>
+<fieldset class="third">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Locale', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-locale-label">
-        <select name="<?php echo $this->plugin_name; ?>[store_locale]" style="width:30%" required>
+        <span><?php esc_html_e('Locale', $this->plugin_name); ?></span>
+    </label>
+    <div class="mailchimp-select-wrapper">
+        <select name="<?php echo $this->plugin_name; ?>[store_locale]" required>
             <?php $selected_locale = isset($options['store_locale']) && !empty($options['store_locale']) ? $options['store_locale'] : 'en'; ?>
             <?php
             foreach(MailChimp_Api_Locales::simple() as $locale_key => $local_value) {
@@ -174,37 +174,37 @@ if (!$handler->hasValidApiKey()) {
             }
             ?>
         </select>
-        <span><?php esc_html_e('Locale', $this->plugin_name); ?></span>
-    </label>
+    </div>
 </fieldset>
 
-<fieldset>
+<fieldset class="third">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Currency Code', 'mc-woocommerce');?></span>
     </legend>
     <?php $all_currencies = MailChimp_WooCommerce_CurrencyCodes::lists();?>
-    
     <label for="<?php echo $this->plugin_name; ?>-store-currency-code-label">
-        <input style="width: 30%;" type="text" value="<?php echo isset($options['store_currency_code']) ? $options['store_currency_code'] . ' | ' . $all_currencies[$options['store_currency_code']]: '' ?>" disabled/>
-        <span><?php esc_html_e('Currency', $this->plugin_name); ?> (<span class="description"><?php esc_html_e('Retrieved from Woocommerce Settings', $this->plugin_name); ?></span>)</span>
+        <span><?php esc_html_e('Woocommerce Currency', $this->plugin_name); ?></span>
     </label>
+    <input type="text" value="<?php echo isset($options['store_currency_code']) ? $options['store_currency_code'] . ' | ' . $all_currencies[$options['store_currency_code']]: '' ?>" disabled/>
 </fieldset>
 
-<fieldset>
+<fieldset class="third">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Timezone', 'mc-woocommerce');?></span>
     </legend>
     <label for="<?php echo $this->plugin_name; ?>-store-timezone-label">
-        <select name="<?php echo $this->plugin_name; ?>[store_timezone]" style="width:30%" required>
-            <?php $selected_timezone = isset($options['store_timezone']) && !empty($options['store_timezone']) ? $options['store_timezone'] : 'America/New_York'; ?>
-            <?php
-             foreach(mailchimp_get_timezone_list() as $t) {
-                echo '<option value="' . esc_attr( $t['zone'] ) . '" ' . selected($t['zone'] === $selected_timezone, true, false ) . '>' . esc_html( $t['diff_from_GMT'] . ' - ' . $t['zone'] ) . '</option>';
-             }
-            ?>
-        </select>
         <span><?php esc_html_e('Timezone', $this->plugin_name); ?></span>
     </label>
+    <div class="mailchimp-select-wrapper">
+        <select name="<?php echo $this->plugin_name; ?>[store_timezone]" required>
+            <?php $selected_timezone = isset($options['store_timezone']) && !empty($options['store_timezone']) ? $options['store_timezone'] : 'America/New_York'; ?>
+            <?php
+                foreach(mailchimp_get_timezone_list() as $t) {
+                echo '<option value="' . esc_attr( $t['zone'] ) . '" ' . selected($t['zone'] === $selected_timezone, true, false ) . '>' . esc_html( $t['diff_from_GMT'] . ' - ' . $t['zone'] ) . '</option>';
+                }
+            ?>
+        </select>
+    </div>
 </fieldset>
 
 

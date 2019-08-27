11cbaf1162e1cc924a70f396066fb821ffd62ac2
Pedro Germani <pedro.germani@gmail.com>
2019-08-27 15:49:50 +0200

    Bump Woocommerce tested version

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -26,7 +26,7 @@
  * Requires at least: 4.9
  * Tested up to: 5.2.2
  * WC requires at least: 3.5
- * WC tested up to: 3.6.5
+ * WC tested up to: 3.7
  */
 
 // If this file is called directly, abort.

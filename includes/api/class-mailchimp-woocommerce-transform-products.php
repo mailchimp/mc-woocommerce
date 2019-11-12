d9dabed384dba201a43112f3565fd3c9f7f9d504
Pedro Germani <pedro.germani@gmail.com>
2019-11-12 16:09:34 +0100

    Check if original vendor is string before applying

--- includes/api/class-mailchimp-woocommerce-transform-products.php
+++ includes/api/class-mailchimp-woocommerce-transform-products.php
@@ -73,7 +73,7 @@ class MailChimp_WooCommerce_Transform_Products
         $vendor_filter = apply_filters('mailchimp_sync_product_vendor', $original_vendor, $product);
         if (is_string($vendor_filter)) {
             $product->setVendor($vendor_filter);
-        } else if ($original_vendor != '') {
+        } else if ($original_vendor != '' && is_string($original_vendor)) {
             $product->setVendor($original_vendor);
         }
 

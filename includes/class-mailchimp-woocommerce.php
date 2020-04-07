86a69acf646ae141ad312526da44b01d6d4f5e24
Miguel Pinto <miguelfspinto@users.noreply.github.com>
2020-04-07 11:02:11 +0100

    option to not render pop-up automatically
    I want to render pop-up but manually.
    So I need to configure it in mailchimp but not trigger it automatically in woocommerce.

--- includes/class-mailchimp-woocommerce.php
+++ includes/class-mailchimp-woocommerce.php
@@ -277,7 +277,9 @@ class MailChimp_WooCommerce
 
 		$plugin_public = new MailChimp_WooCommerce_Public( $this->get_plugin_name(), $this->get_version() );
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
-        $this->loader->add_action('wp_footer', $plugin_public, 'add_inline_footer_script');
+		if ( apply_filters( 'mailchimp_add_inline_footer_script', true ) ) {
+        		$this->loader->add_action('wp_footer', $plugin_public, 'add_inline_footer_script');
+		}
 	}
 
 	/**

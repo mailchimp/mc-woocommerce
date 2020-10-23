89f9e3bae675b1783490f5d762216c986e54bd35
Pedro Germani <pedro.germani@gmail.com>
2020-10-23 10:34:27 +0200

    Re-skin: re-structure ssetttings header

--- admin/partials/tabs/api_key.php
+++ admin/partials/tabs/api_key.php
@@ -1,14 +1,9 @@
 <input type="hidden" name="mailchimp_active_settings_tab" value="api_key"/>
-
-<!-- remove some meta and generators from the <head> -->
 <fieldset class="full">
     <legend class="screen-reader-text">
         <span><?php esc_html_e('Connect your store to Mailchimp', 'mailchimp-for-woocommerce');?></span>
     </legend>
-    
-    
     <a id="mailchimp-oauth-connect" class="button button-primary tab-content-submit oauth-connect"><?php $has_valid_api_key ? esc_html_e('Reconnect', 'mailchimp-for-woocommerce') : esc_html_e('Connect', 'mailchimp-for-woocommerce');?></a>
-    <h4><?php esc_html_e('Connect your store to Mailchimp', 'mailchimp-for-woocommerce'); ?></h4>
     <input type="hidden" id="<?php echo $this->plugin_name; ?>-mailchimp-api-key" name="<?php echo $this->plugin_name; ?>[mailchimp_api_key]" value="<?php echo isset($options['mailchimp_api_key']) ? $options['mailchimp_api_key'] : '' ?>" required/>
     <?php if ($has_valid_api_key) :?>
         <p id="mailchimp-oauth-api-key-valid"><?php esc_html_e('Already connected. You can reconnect with another Mailchimp account if you want.' , 'mailchimp-for-woocommerce');?></p>
@@ -18,4 +13,3 @@
     <p id="mailchimp-oauth-connecting" class="oauth-description"><?php esc_html_e('Connection in progress' , 'mailchimp-for-woocommerce');?></p>
     <p id="mailchimp-oauth-connected" class="oauth-description "><?php esc_html_e('Connected! Please wait while loading next step', 'mailchimp-for-woocommerce');?></p>
 </fieldset>
-

739096df51ac4b028f7593496e01998a37944c48
Ryan Hungate <ryanhungate@icloud.com>
2020-12-03 08:24:09 -0700

    update version 2.5

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Tags: ecommerce,email,workflows,mailchimp
 Donate link: https://mailchimp.com
 Requires at least: 4.9
 Tested up to: 5.6
-Stable tag: 2.4.7
+Stable tag: 2.5.0
 Requires PHP: 7.0
 WC requires at least: 3.5
 WC tested up to: 4.7
--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -24,9 +24,9 @@
  * Text Domain:       mailchimp-for-woocommerce
  * Domain Path:       /languages
  * Requires at least: 4.9
- * Tested up to: 5.5
+ * Tested up to: 5.6
  * WC requires at least: 3.5
- * WC tested up to: 4.4
+ * WC tested up to: 4.7
  */
 
 // If this file is called directly, abort.

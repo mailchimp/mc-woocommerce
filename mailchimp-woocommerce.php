cec96216cdc45d8976cef7cff2692b8886939a53
Viktor Szépe <viktor@szepe.net>
2021-04-18 21:41:51 +0200

    Load plugin files from an absolute path

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -24,9 +24,9 @@
  * Text Domain:       mailchimp-for-woocommerce
  * Domain Path:       /languages
  * Requires at least: 4.9
- * Tested up to: 5.7
+ * Tested up to:      5.7
  * WC requires at least: 3.5
- * WC tested up to: 5.1
+ * WC tested up to:   5.1
  */
 
 // If this file is called directly, abort.
@@ -35,13 +35,13 @@ if (!defined( 'WPINC')) {
 }
 
 if (!isset($mailchimp_woocommerce_spl_autoloader) || $mailchimp_woocommerce_spl_autoloader === false) {
-    // require Action Scheduler
-    include_once "includes/vendor/action-scheduler/action-scheduler.php";
-    // bootstrapper
-    include_once "bootstrap.php";
+    // Require Action Scheduler.
+    require_once __DIR__ . '/includes/vendor/action-scheduler/action-scheduler.php';
+    // Load bootstrapper.
+    require_once __DIR__ . '/bootstrap.php';
 }
 
-register_activation_hook( __FILE__, 'activate_mailchimp_woocommerce');
+register_activation_hook(__FILE__, 'activate_mailchimp_woocommerce');
 
-// plugins loaded callback
+// Start plugin at `plugins_loaded`.
 add_action('plugins_loaded', 'mailchimp_on_all_plugins_loaded', 12);

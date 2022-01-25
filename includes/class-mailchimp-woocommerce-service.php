46d8d1391f0159e3b0fe3204f647e999b822fd40
Ryan Hungate <ryanhungate@icloud.com>
2022-01-25 12:11:05 -0700

    remove subscribe from cart

--- includes/class-mailchimp-woocommerce-service.php
+++ includes/class-mailchimp-woocommerce-service.php
@@ -266,7 +266,7 @@ class MailChimp_Service extends MailChimp_WooCommerce_Options
                 $handler = new MailChimp_WooCommerce_Cart_Update($uid, $user_email, $campaign, $this->cart, $language);
 
                 // if they had the checkbox checked - go ahead and subscribe them if this is the first post.
-                $handler->setStatus($this->cart_subscribe);
+                //$handler->setStatus($this->cart_subscribe);
 
                 mailchimp_handle_or_queue($handler);
             }

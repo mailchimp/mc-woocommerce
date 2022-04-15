da8ca0082b7f7af739d70c26c2e5e565a0709f8e
Alejanro Giraldo <alejandro@vextras.com>
2022-04-15 16:09:09 -0500

    add opt in preference on My Account page [sc-30743]

--- public/partials/mailchimp-woocommerce-my-account.php
+++ public/partials/mailchimp-woocommerce-my-account.php
@@ -2,3 +2,5 @@
 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     <input type="checkbox" name="mailchimp_woocommerce_is_subscribed_checkbox" id="mailchimp_woocommerce_is_subscribed" <?php echo esc_attr( get_user_meta($user->ID, 'mailchimp_woocommerce_is_subscribed', true)  ? 'checked' : '') ?> > <?php esc_html_e('Subscribe Newsletter', 'mailchimp-for-woocommerce')?>
 </p>
+
+<?php echo $gdpr_fields ?>

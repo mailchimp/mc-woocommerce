5438eb790ec786119403dee09b917fef78500e1a
Ryan Hungate <ryanhungate@icloud.com>
2022-08-01 07:44:15 -0700

    make sure the integraion interface exists

--- blocks/newsletter.php
+++ blocks/newsletter.php
@@ -2,7 +2,8 @@
 
 define( 'MAILCHIMP_WOOCOMMERCE_NEWSLETTER_VERSION', '1.0.0' );
 
-if ( class_exists( '\Automattic\WooCommerce\Blocks\Package' ) ) {
+if (class_exists( '\Automattic\WooCommerce\Blocks\Package' ) &&
+    class_exists('\Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface')) {
 	require dirname( __FILE__ ) . '/woocommerce-blocks-integration.php';
 	add_action(
 		'woocommerce_blocks_checkout_block_registration',

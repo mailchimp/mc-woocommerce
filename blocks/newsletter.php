2deb67358abcf1a5d2b0a6f45113246f76ff6865
Ryan Hungate <ryanhungate@icloud.com>
2022-08-01 08:57:18 -0700

    checking for interface - not class.

--- blocks/newsletter.php
+++ blocks/newsletter.php
@@ -3,7 +3,7 @@
 define( 'MAILCHIMP_WOOCOMMERCE_NEWSLETTER_VERSION', '1.0.0' );
 
 if (class_exists( '\Automattic\WooCommerce\Blocks\Package' ) &&
-    class_exists('\Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface')) {
+    interface_exists('\Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface')) {
 	require dirname( __FILE__ ) . '/woocommerce-blocks-integration.php';
 	add_action(
 		'woocommerce_blocks_checkout_block_registration',

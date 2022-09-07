c44334962a703653076eefa64a588be10ff621e6
Ryan Hungate <ryanhungate@icloud.com>
2022-09-07 09:30:24 -0700

    How to use mailchimp woocommerce newsletter [sc-31624]

--- public/partials/mailchimp-woocommerce-my-account.php
+++ public/partials/mailchimp-woocommerce-my-account.php
@@ -1,13 +1,15 @@
 <?php $user = wp_get_current_user(); ?>
 <?php $mailchimp_user_is_subscribed = ($user && $user->ID) ? get_user_meta($user->ID, 'mailchimp_woocommerce_is_subscribed', true) : false; ?>
-
-<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-    <input type="checkbox"
-           class='woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'
-           name="mailchimp_woocommerce_is_subscribed_checkbox"
-           id="mailchimp_woocommerce_is_subscribed"
-        <?php echo !empty($mailchimp_user_is_subscribed) ? esc_attr('checked') : ''; ?>>
-    <?php esc_html_e('Subscribe Newsletter', 'mailchimp-for-woocommerce')?>
-</p>
-
+<?php
+$mailchimp_my_account_label = esc_html( translate( 'Subscribe to our newsletter', 'mailchimp-for-woocommerce' ) );
+$mailchimp_my_account = '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
+$mailchimp_my_account .= '<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="mailchimp_woocommerce_is_subscribed_checkbox" id="mailchimp_woocommerce_is_subscribed" ';
+$mailchimp_my_account .= !empty($mailchimp_user_is_subscribed) ? esc_attr('checked') : '';
+$mailchimp_my_account .= '>';
+$mailchimp_my_account .= $mailchimp_my_account_label;
+$mailchimp_my_account .= translate( 'Subscribe to our newsletter', 'mailchimp-for-woocommerce' );
+$mailchimp_my_account .= '</p>';
+echo wp_kses_post( apply_filters( 'mailchimp_woocommerce_my_account_field', $mailchimp_my_account, $mailchimp_user_is_subscribed, $mailchimp_my_account_label ) );
+?>
 <?php if (isset($gdpr_fields) && $gdpr_fields) { echo wp_kses_post($gdpr_fields); } ?>
+

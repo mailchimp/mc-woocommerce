4209db256a59b9f0d334e942d05a5b17a2481e74
DANYLO <agafonec1@gmail.com>
2023-03-03 12:30:13 +0200

    2.8.1 hide the subscrbie block from my-account view if status is archived. and keep the status archived.

--- public/partials/mailchimp-woocommerce-my-account.php
+++ public/partials/mailchimp-woocommerce-my-account.php
@@ -1,9 +1,7 @@
 <?php
 $user = wp_get_current_user();
 $mailchimp_user_subscription_status = ($user && $user->ID) ? get_user_meta($user->ID, 'mailchimp_woocommerce_is_subscribed', true) : false;
-
 if ($mailchimp_user_subscription_status !== false && $mailchimp_user_subscription_status !== 'archived') {
-
     $mailchimp_my_account = '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
     $mailchimp_my_account .= '<label for="mailchimp_woocommerce_is_subscribed">';
     $mailchimp_my_account .= '<input type="radio" class="woocommerce-form__input woocommerce-form__input-radio input-radio" name="mailchimp_woocommerce_is_subscribed_radio" id="mailchimp_woocommerce_is_subscribed" value="1"';
@@ -32,14 +30,15 @@ if ($mailchimp_user_subscription_status !== false && $mailchimp_user_subscriptio
     $mailchimp_my_account .= '</p>';
 
 
-	echo wp_kses(
-		apply_filters( 'mailchimp_woocommerce_my_account_field', $mailchimp_my_account, $mailchimp_user_subscription_status ),
-		mailchimp_expanded_alowed_tags()
-	);
-	if (isset($gdpr_fields) && $gdpr_fields) {
-		echo wp_kses($gdpr_fields, mailchimp_expanded_alowed_tags());
-	}
+    echo wp_kses(
+        apply_filters( 'mailchimp_woocommerce_my_account_field', $mailchimp_my_account, $mailchimp_user_subscription_status ),
+        mailchimp_expanded_alowed_tags()
+    );
+    if (isset($gdpr_fields) && $gdpr_fields) {
+        echo wp_kses($gdpr_fields, mailchimp_expanded_alowed_tags());
+    }
 } else {
-	echo '<input type="hidden" name="mailchimp_woocommerce_is_subscribed_radio" id="mailchimp_woocommerce_is_subscribed" value="1">';
+    echo '<input type="hidden" name="mailchimp_woocommerce_is_subscribed_radio" id="mailchimp_woocommerce_is_subscribed" value="' . ($mailchimp_user_subscription_status === 'archived' ? 'archived' : '1') . '">';
 }
+
 ?>

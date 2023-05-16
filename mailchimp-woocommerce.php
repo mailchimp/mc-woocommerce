0c39ddc2329f34aaa957f9e169de4aeae4314bd1
Ryan Hungate <ryanhungate@icloud.com>
2023-05-16 13:59:45 -0400

    3.0 for HPOS major release upgrade

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Mailchimp for WooCommerce
  * Plugin URI:        https://mailchimp.com/connect-your-store/
  * Description:       Connects WooCommerce to Mailchimp to sync your store data, send targeted campaigns to your customers, and sell more stuff. 
- * Version:           2.8.4
+ * Version:           3.0
  * Author:            Mailchimp
  * Author URI:        https://mailchimp.com
  * License:           GPL-2.0+

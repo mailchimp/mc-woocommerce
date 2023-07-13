141f8c518b546a4f4a2b084ce5109ca7939036ff
Ryan Hungate <ryanhungate@icloud.com>
2023-07-13 11:58:35 -0400

    update dependencies on block editor

--- blocks/build/newsletter-block.asset.php
+++ blocks/build/newsletter-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c1886bed13e6141631302ef7bcf439cd');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fbb04874706ade6dceb1b2b8e0287bd1');

688544c03e3448fbcf9794fa683fa37fc6cfac4e
Ryan Hungate <ryanhungate@icloud.com>
2023-07-20 09:49:26 -0400

    update plugin meta

--- mailchimp-woocommerce.php
+++ mailchimp-woocommerce.php
@@ -24,9 +24,9 @@
  * Text Domain:       mailchimp-for-woocommerce
  * Domain Path:       /languages
  * Requires at least: 4.9
- * Tested up to: 6.2
+ * Tested up to: 6.3
  * WC requires at least: 3.5
- * WC tested up to: 7.6
+ * WC tested up to: 7.9
  */
 
 // If this file is called directly, abort.

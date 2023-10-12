db4601647854be028fc24783132594ff17d43f00
tarassych <taras.sych@gmail.com>
2023-10-12 14:42:56 +0300

    Removed text and added visual enhancements

--- blocks/build/newsletter-block.asset.php
+++ blocks/build/newsletter-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fa68e943aa8bbd62c918');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b7e427ee568cf4aabe53');

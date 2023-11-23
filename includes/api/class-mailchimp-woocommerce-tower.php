a0e0b54b7f6ffa9136b2c87acc6b05a52af9cb6a
tarassych <taras.sych@gmail.com>
2023-11-23 10:23:10 +0200

    update is_shell_enabled method

--- includes/api/class-mailchimp-woocommerce-tower.php
+++ includes/api/class-mailchimp-woocommerce-tower.php
@@ -780,7 +780,7 @@ class MailChimp_WooCommerce_Tower extends Mailchimp_Woocommerce_Job {
 		/*Check if shell_exec() is enabled on this server*/
 		if ( function_exists( 'shell_exec' ) && ! in_array( 'shell_exec', array_map( 'trim', explode( ', ', ini_get( 'disable_functions' ) ) ) ) ) {
 			/*If enabled, check if shell_exec() actually have execution power*/
-			$returnVal = shell_exec( 'cat /proc/cpuinfo' );
+			$returnVal = shell_exec( 'pwd' );
 			if ( ! empty( $returnVal ) ) {
 				return true;
 			}

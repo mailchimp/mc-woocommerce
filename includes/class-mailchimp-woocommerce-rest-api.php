d73986dd6f2124be2528bc54978593a0008ba47c
tarassych <taras.sych@gmail.com>
2024-02-22 09:57:42 +0200

    fixed issue when order return boolean

--- includes/class-mailchimp-woocommerce-rest-api.php
+++ includes/class-mailchimp-woocommerce-rest-api.php
@@ -507,8 +507,8 @@ class MailChimp_WooCommerce_Rest_Api
             case 'order':                
                 $order = MailChimp_WooCommerce_HPOS::get_order($body['resource_id']);
                 /*$order = get_post($body['resource_id']);*/
-                $mc = !$order->get_id() ? null : mailchimp_get_api()->getStoreOrder($store_id, $order->get_id());
-                if ($order->get_id()) {
+                if ($order && $order->get_id()) {
+                    $mc = mailchimp_get_api()->getStoreOrder($store_id, $order->get_id());
                     $transformer = new MailChimp_WooCommerce_Transform_Orders();
                     $platform = $transformer->transform($order)->toArray();
                 }

5dc2fc9836e8c6cf706809a1d15c4445d12634bf
tarassych <taras.sych@gmail.com>
2024-02-26 16:46:57 +0200

    Updated dependencies

--- blocks/build/newsletter-block-frontend.asset.php
+++ blocks/build/newsletter-block-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-shared-hocs', 'wc-settings', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6c38ea938266abe7c582');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-shared-hocs', 'wc-settings', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9928316618b9da0d4020');

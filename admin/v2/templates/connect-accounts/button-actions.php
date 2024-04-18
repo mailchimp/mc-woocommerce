7bad6aaed43fd7b6a6a72986c96676b0881c4cf2
Ryan Hungate <ryanhungate@icloud.com>
2024-04-18 13:49:13 -0400

    remove message and reconnect button

--- admin/v2/templates/connect-accounts/button-actions.php
+++ admin/v2/templates/connect-accounts/button-actions.php
@@ -13,13 +13,10 @@ $show_connection_messages = false;
 	</legend>
 	<div class="mc-wc-actions">
 		<a class="mc-wc-btn mc-wc-btn-primary-outline create-account" href='#mc-woocommerce-create-account'><?php esc_html_e( 'Create account', 'mailchimp-for-woocommerce' ); ?></a>
-		<a id="mailchimp-oauth-connect" class="mc-wc-btn mc-wc-btn-primary oauth-connect"><?php isset( $has_valid_api_key ) && $has_valid_api_key ? esc_html_e( 'Reconnect', 'mailchimp-for-woocommerce' ) : esc_html_e( 'Connect', 'mailchimp-for-woocommerce' ); ?></a>
+		<a id="mailchimp-oauth-connect" class="mc-wc-btn mc-wc-btn-primary oauth-connect"><?php esc_html_e( 'Connect', 'mailchimp-for-woocommerce' );  ?></a>
 	</div>
 	
 	<input type="hidden" id="<?php echo esc_attr( $this->plugin_name ); ?>-mailchimp-api-key" name="<?php echo esc_attr( $this->plugin_name ); ?>[mailchimp_api_key]" value="<?php echo isset( $options['mailchimp_api_key'] ) ? esc_html( $options['mailchimp_api_key'] ) : ''; ?>" required/>
-	<?php if ( isset( $has_valid_api_key ) && $has_valid_api_key ) : ?>
-		<p id="mailchimp-oauth-api-key-valid"><?php esc_html_e( 'Already connected. You can reconnect with another Mailchimp account if you want.', 'mailchimp-for-woocommerce' ); ?></p>
-	<?php endif; ?>
     <?php if ($show_connection_messages) : ?>
 	<p id="mailchimp-oauth-waiting" class="oauth-description"><?php esc_html_e( 'Connecting. A new window will open with Mailchimp\'s OAuth service. Please log-in and we will take care of the rest.', 'mailchimp-for-woocommerce' ); ?></p>
 	<p id="mailchimp-oauth-error" class="oauth-description"><?php esc_html_e( 'Error, can\'t login.', 'mailchimp-for-woocommerce' ); ?></p>

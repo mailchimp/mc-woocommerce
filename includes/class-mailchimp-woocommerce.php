0b65b67e768a028fe51e0c7d662e533db123c9e4
Ryan Hungate <ryanhungate@icloud.com>
2024-05-22 14:55:56 -0400

    freddie on the sidebar

--- includes/class-mailchimp-woocommerce.php
+++ includes/class-mailchimp-woocommerce.php
@@ -230,7 +230,7 @@ class MailChimp_WooCommerce
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
 
 		// Add menu item
-		$this->loader->add_action('admin_menu', $plugin_admin, 'add_plugin_admin_menu_2', 71);
+		$this->loader->add_action('admin_menu', $plugin_admin, 'add_plugin_admin_menu', 71);
         $this->loader->add_action('admin_menu', $plugin_admin, 'add_create_account_page', 72);
 
         // Add WooCommerce Navigation Bar
